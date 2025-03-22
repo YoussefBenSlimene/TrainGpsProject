df3932376b66322ab7d0d5f903f89c651ab06bd7
YoussefBenSlimene <benslimeneyoussef@mahdia.r-iset.tn>
2025-03-22 22:07:08 +0100

    first commit

---
+++ src/main/java/iset/traingpsbackend/TrainGpsBackendApplication.java
@@ -0,0 +1,13 @@
+package iset.traingpsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainGpsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainGpsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iset/traingpsbackend/TrainGpsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package iset.traingpsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainGpsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
