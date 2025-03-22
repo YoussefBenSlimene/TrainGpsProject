ba61bd4c8f0b9da4866c9f201ef1629959bb2ecf
YoussefBenSlimene <benslimeneyoussef@mahdia.r-iset.tn>
2025-03-22 23:32:26 +0100

    first commit

--- src/main/java/iset/traingpsbackend/TrainGpsBackendApplication.java
+++ src/main/java/iset/traingpsbackend/TrainGpsBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TrainGpsBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TrainGpsBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TrainGpsBackendApplication.class, args);
+    }
 
 }
