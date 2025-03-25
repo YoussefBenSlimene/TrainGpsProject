f9791392aa6393c5c1729ee9fa94ea0937e39861
YoussefBenSlimene <benslimeneyoussef@mahdia.r-iset.tn>
2025-03-25 16:15:34 +0100

    latest update

--- src/main/java/iset/traingpsbackend/controller/LocateController.java
+++ src/main/java/iset/traingpsbackend/controller/LocateController.java
@@ -1,27 +1,45 @@
 package iset.traingpsbackend.controller;
 
 import iset.traingpsbackend.model.Locate;
-import org.slf4j.LoggerFactory;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
 @Controller
 public class LocateController {
     private static final Logger logger = LoggerFactory.getLogger(LocateController.class);
 
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public LocateController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
     @MessageMapping("/locate")
-    @SendTo("/topic/locate")
-    public Locate locate(Locate location) throws Exception {
-        Thread.sleep(1000);
+    @SendTo("/topic/location")
+    public Locate handleLocation(Locate location) {
+        // Process and return the location
         return location;
     }
 
-    @MessageMapping("/message")
-    @SendTo("/topic/messages")
-    public String handleMessage(String message) {
-        logger.info("Received message: {}", message);
-        return "Server received: " + message;
+    @MessageMapping("/locate-json")
+    @SendTo("/topic/locate")
+    public String locateJson(@Payload String jsonMessage) {
+        if (jsonMessage == null || jsonMessage.isBlank()) {
+            logger.error("Received empty JSON message!");
+            return "{}";
+        }
+
+        logger.info("Received JSON message: {}", jsonMessage);
+        return jsonMessage;
+    }
+
+    public void sendLocationUpdate(Locate location) {
+        logger.info("Sending location update: {}", location);
+        messagingTemplate.convertAndSend("/topic/locate", location);
     }
 }
